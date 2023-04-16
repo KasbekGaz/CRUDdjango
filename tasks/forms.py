0871a1b33a5119eb1488fc49cdf5d7d58144415c
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-04-15 19:30:26 -0600

    Estilizar con Bootstrap5

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,9 +1,17 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Task #importamos el modelo que vamos usar
 
 
-class TaskForm(ModelForm):  #creamos las clase
+class TaskForm(forms.ModelForm):  #creamos las clase
     class Meta:   #introducimos una clase meta para procesar datos
         model = Task  #referenciamos el modelo tarea.
         fields = ['titulo', 'descripcion', 'importante']
+        widgets = { #esto es un diccionario para mandar un formulario en la vista create_tasks.html
+            'titulo': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Titulo de la tarea.'}),
+            'descripcion': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Describe la tarea.'}),
+            #No logramos poner el checkbox desde aqui
+            #'importante': forms.CheckboxInput(attrs={'class':'form-control'}),
+            #Lo que hizo fue poner otra clase
+            'importante': forms.CheckboxInput(attrs={'class':'form-check-input'}),
+        }
 
